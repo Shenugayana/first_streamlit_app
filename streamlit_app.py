30a265d6e505547e449981119fe75bb13e4a8c35
Shenugayana <66559298+Shenugayana@users.noreply.github.com>
2022-08-10 11:46:34 +0530

    Update streamlit_app.py
    textbox added

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("👩‍🍳 Fruityvice Fruit Advice! 👨‍🌾")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
